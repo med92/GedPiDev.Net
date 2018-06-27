3e2fd587268ba35413c9951d60d86a258a9334db
mehdi-lkdr <mehdii_lakhdar@hotmail.com>
2018-06-27 22:50:58 +0100

    init commit

---
+++ GedPiDev.Data/GedPiDevContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MySql.Data.Entity;
+
+namespace GedPiDev.Data
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class GedPiDevContext : DbContext
+    {
+        public GedPiDevContext() : base()
+        {
+
+        }
+    }
+}
