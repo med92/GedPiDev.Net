16805e31f69b20e255acaa0a40dcf8b0dd3173c4
Mohamed Nefzi EXT <mohamed.nefzi-ext@be-softilys.tn>
2018-06-30 18:13:57 +0200

    add pattern and Services

---
+++ GedPiDev.Data/Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,27 @@
+﻿using GedPidev.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GedPiDev.Data.Infrastructure
+{
+    public class DatabaseFactory : Disposable, IDatabaseFactory
+    {
+        private GedPiDevContext dataContext;
+        public GedPiDevContext DataContext { get { return dataContext; } }
+
+        public DatabaseFactory()
+        {
+            dataContext = new GedPiDevContext();
+        }
+        protected override void DisposeCore()
+        {
+            if (DataContext != null)
+                DataContext.Dispose();
+        }
+    }
+
+}
---
+++ GedPiDev.Data/Infrastructure/IDatabaseFactory .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GedPiDev.Data.Infrastructure
+{
+    public interface IDatabaseFactory : IDisposable
+    {
+        GedPiDevContext DataContext { get; }
+    }
+
+}
