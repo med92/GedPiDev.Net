5655576389c745878cb88946671d0de58de54480
Mohamed Nefzi EXT <mohamed.nefzi-ext@be-softilys.tn>
2018-06-30 18:30:00 +0200

    Fix Pattern Project

---
+++ GedPiDev.RestAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GedPiDev.RestAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
